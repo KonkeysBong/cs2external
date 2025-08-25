aa1f4945f972039cfa5726b0c676da2ee49595ee
FetteEnte <137723155+KonkeysBong@users.noreply.github.com>
2025-08-26 00:10:28 +0200

    external hackclient for cs2

---
+++ cs2_dumper/output/offsets.rs
@@ -0,0 +1,56 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2025-07-31 16:21:55.223367600 UTC
+
+#![allow(non_upper_case_globals, unused)]
+
+pub mod cs2_dumper {
+    pub mod offsets {
+        // Module: client.dll
+        pub mod client_dll {
+            pub const dwCSGOInput: usize = 0x1D2C090;
+            pub const dwEntityList: usize = 0x1CBE6A0;
+            pub const dwGameEntitySystem: usize = 0x1E01C60;
+            pub const dwGameEntitySystem_highestEntityIndex: usize = 0x20F0;
+            pub const dwGameRules: usize = 0x1D1D560;
+            pub const dwGlobalVars: usize = 0x1AE9458;
+            pub const dwGlowManager: usize = 0x1D1D588;
+            pub const dwLocalPlayerController: usize = 0x1D102C0;
+            pub const dwLocalPlayerPawn: usize = 0x1AF4B80;
+            pub const dwPlantedC4: usize = 0x1D27200;
+            pub const dwPrediction: usize = 0x1AF4AB0;
+            pub const dwSensitivity: usize = 0x1D1E098;
+            pub const dwSensitivity_sensitivity: usize = 0x48;
+            pub const dwViewAngles: usize = 0x1D2C840;
+            pub const dwViewMatrix: usize = 0x1D21A80;
+            pub const dwViewRender: usize = 0x1D226C0;
+            pub const dwWeaponC4: usize = 0x1CC1908;
+        }
+        // Module: engine2.dll
+        pub mod engine2_dll {
+            pub const dwBuildNumber: usize = 0x5B94C4;
+            pub const dwNetworkGameClient: usize = 0x5B85B0;
+            pub const dwNetworkGameClient_clientTickCount: usize = 0x358;
+            pub const dwNetworkGameClient_deltaTick: usize = 0x23C;
+            pub const dwNetworkGameClient_isBackgroundMap: usize = 0x2C142F;
+            pub const dwNetworkGameClient_localPlayer: usize = 0xE8;
+            pub const dwNetworkGameClient_maxClients: usize = 0x230;
+            pub const dwNetworkGameClient_serverTickCount: usize = 0x23C;
+            pub const dwNetworkGameClient_signOnState: usize = 0x220;
+            pub const dwWindowHeight: usize = 0x8AB4DC;
+            pub const dwWindowWidth: usize = 0x8AB4D8;
+        }
+        // Module: inputsystem.dll
+        pub mod inputsystem_dll {
+            pub const dwInputSystem: usize = 0x459F0;
+        }
+        // Module: matchmaking.dll
+        pub mod matchmaking_dll {
+            pub const dwGameTypes: usize = 0x1B3F10;
+        }
+        // Module: soundsystem.dll
+        pub mod soundsystem_dll {
+            pub const dwSoundSystem: usize = 0x3CF620;
+            pub const dwSoundSystem_engineViewData: usize = 0x7C;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,22 @@
+## 1. Download and Set Up CS2 Dumper
+Download the CS2 Dumper (https://github.com/a2x/cs2-dumper/releases).
+Extract the .exe file and place it in the folder with the name "cs2_dumper".
+
+## 2. Start CS2 in Insecure Mode
+Launch CS2 with the -insecure mode enabled. This ensures that you won't get banned.
+
+## 3. Run CS2 Dumper
+Open the cs2-dumper.exe file from the cs2_dumper folder.
+
+## 4. Close CS2 and Restart Normally
+After running the dumper, close CS2.
+You can now restart the game in normal mode and use the hack.
+
+## 5. Using the Menu
+Once in the game, use the hack menu.
+Press F8 to run the menu in the background when you're done so you can't interact with it further.
+
+## Updating Offsets
+If the offsets are broken, press the "Update Offset" button.
+It's better to perform this update in -insecure mode, but you are unlikely to be banned if you don't.
+Sometimes you have to download a newer version of the dumper.
